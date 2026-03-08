13924d9e6a22015d7a970a80ee505ca6e8559504
patrikalaimo-cmyk <patrik.alaimo@gmail.com>
2026-03-08 22:58:46 +0100

    Update Supabase client configuration with new URL and key

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "INSERISCI_URL_SUPABASE"
-const supabaseKey = "INSERISCI_ANON_KEY"
+const supabaseUrl = "https://nhddvvategpthkchylfs.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oZGR2dmF0ZWdwdGhrY2h5bGZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMDQ0MDMsImV4cCI6MjA4ODU4MDQwM30.tMsdUvUrEVQFDXRhvp9adgqQ3YRqdntcMoULj_tQaPw"
 
 export const supabase = createClient(supabaseUrl,supabaseKey)
